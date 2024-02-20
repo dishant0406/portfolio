fc49127939603df5604bcb523359f72937d92920
dishant <dishu5570@gmail.com>
2024-02-20 11:34:42 +0530

    portfolio

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -4,6 +4,7 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head />
+      <script defer src="https://analytics.us.umami.is/script.js" data-website-id="f85088d6-f79d-45a1-844e-45417e551ac4"></script>
       <body>
         <Main />
         <NextScript />
